ddb326e6ac817265c96ddb502da2c72fc6e99da8
LeomaraAC <leomara.andrade@gmail.com>
2019-01-11 16:59:04 -0200

    Produto e associação muito para muitos

---
+++ src/main/java/com/leomara/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.leomara.cursomc.repositories;
+
+import com.leomara.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
