bb464674d062ac5b806bbd9a205ce747d4ce0c8d
LeomaraAC <leomara.andrade@gmail.com>
2019-03-03 13:23:37 -0300

    Atualização: utilizando somente JsonIgnore

--- src/main/java/com/leomara/cursomc/domain/Endereco.java
+++ src/main/java/com/leomara/cursomc/domain/Endereco.java
@@ -1,6 +1,6 @@
 package com.leomara.cursomc.domain;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -19,10 +19,11 @@ public class Endereco implements Serializable {
     private String bairro;
     private String cep;
 
-    @JsonBackReference
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
+
     @ManyToOne
     @JoinColumn(name = "cidade_id")
     private Cidade cidade;
