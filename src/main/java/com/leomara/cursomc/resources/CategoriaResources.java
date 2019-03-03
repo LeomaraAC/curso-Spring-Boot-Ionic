5c03f049428f7cb7bc833fac65132b35296b3656
LeomaraAC <leomara.andrade@gmail.com>
2019-03-03 14:30:43 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/leomara/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/leomara/cursomc/resources/CategoriaResources.java
@@ -5,7 +5,9 @@ import com.leomara.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,5 +22,12 @@ public class CategoriaResources {
     public ResponseEntity<?> find(@PathVariable Integer id) {
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
